e780d1817c3e403b46c0791a64873aeb28c00e3e
Tanner Donovan <ttdonovan@gmail.com>
2013-04-04 22:48:42 -0700

    cleanup development setup and added 'development setup' instructions to the README.md

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Stardust::Application.config.secret_token = 'd2a51dc07b7ca6599b9f3d9ae8fe6355c664a01360d4bd5ba9868ff4ce3f70bb2dcba8d4bd0327024b0dd51ce64ec53509e9ec2603954381d06335b252a03da9'
+Stardust::Application.config.secret_token = ENV['SECRET_TOKEN']
